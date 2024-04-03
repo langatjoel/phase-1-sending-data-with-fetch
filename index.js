f530a4c0d27182b2e36c4ba901267db3e17ce506
joel langat <langatjoel42@gmail.com>
2024-04-03 20:56:16 +0300

    made some functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObject)
+      .then(response => response.json())
+      .then(data => {
+        document.body.innerHTML += `<p>${data.id}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>${error.message}</p>`;
+      });
+  }
